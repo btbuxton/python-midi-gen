0397e2549f5d1065ed55657cff91d53e89c39262
Blaine Buxton <btbuxton@gmail.com>
2016-07-16 10:43:01 -0400

    added equals test for note

--- tests/midi_gen/midi/test_engine.py
+++ tests/midi_gen/midi/test_engine.py
@@ -1,15 +1,17 @@
 import unittest
 
 from midi_gen.midi.engine import Note
+from test.test_support import get_attribute
 
 class NoteTest(unittest.TestCase):
 
     def testNote(self):
-        note = Note.C2
+        note = Note(24)
         self.assertEqual(24, note.number)
+        self.assertEqual(note, get_attribute(Note, 'C2'))
         
     def testOctaveUp(self):
-        note = Note.C2
+        note = Note(24)
         self.assertEqual(36, note.octave_up().number)
     
     def testOctaveDownFail(self):
