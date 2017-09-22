2b15ac8d8f8affbef478845963df2900fcb7a990
Blaine Buxton <btbuxton@gmail.com>
2017-09-22 15:51:31 -0400

    more python3 changes

--- bin/display_midi_devices.py
+++ bin/display_midi_devices.py
@@ -2,10 +2,10 @@ from pygame import midi
 
 
 def main():
-    print 'Number of devices: %d' % midi.get_count()
-    print 'Default input id: %d' % midi.get_default_input_id()
-    print 'Default output id: %d' % midi.get_default_output_id()
-    for index in xrange(midi.get_count()):
+    print('Number of devices: %d' % midi.get_count())
+    print('Default input id: %d' % midi.get_default_input_id())
+    print('Default output id: %d' % midi.get_default_output_id())
+    for index in range(midi.get_count()):
         device = midi.get_device_info(index)
         if 1 == device[2]:
             input_dev = 'in'
@@ -16,7 +16,7 @@ def main():
         else:
             output_dev = '-'
             
-        print 'Device id: %d - %s (%r,%r)' % (index, device[1], input_dev, output_dev)
+        print('Device id: %d - %s (%r,%r)' % (index, device[1], input_dev, output_dev))
 if __name__ == '__main__':
     midi.init()
     main()
